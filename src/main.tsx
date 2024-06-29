cee46d2ebe1e8768ca61ed850dedb0cafae2005a
ChunEunyu <xhoneyx01@naver.com>
2024-06-29 22:48:09 +0900

    fix: createBrowserRouter와 createRoutesFromElements 사용

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,42 @@
-import { QueryClientProvider, QueryClient } from "react-query";
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import App from './App.tsx';
+import { QueryClientProvider, QueryClient } from "react-query";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { 
+  Auth,
+  Daily,
+  Finder,
+  Home,
+  Membership,
+  Themes, 
+} from "./pages";
 import '@/styles/globalStyle.css';
+import Root from "./Root";
 
 const queryClient = new QueryClient(); 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/daily" element={<Daily />} />
+      <Route path="/finder" element={<Finder />} />
+      <Route path="/membership" element={<Membership />} />
+      <Route path="/themes" element={<Themes />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
